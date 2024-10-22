6df943da9f06a1d88c36d79e655c143e211ad563
Naavin07 <naavinbalayah@gmail.com>
2024-10-22 15:56:44 +0800

    Added SpringSecurity configuration setup

---
+++ src/main/java/com/example/Aon/security/SpringSecurity.java
@@ -0,0 +1,27 @@
+package com.example.Aon.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SpringSecurity {
+
+    private static final String[] SWAGGER_PATHS = {"/swagger-ui.html", "/v3/api-docs/**", "swagger-ui/**", "/webjars/swagger-ui/**"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorize ->authorize
+                        .requestMatchers(SWAGGER_PATHS).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated()
+                ).build();
+    }
+}
